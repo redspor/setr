c21af64ecc158fd7d97ba1e0c407cc5b30c25058
redspor <121644986+redspor@users.noreply.github.com>
2022-12-30 04:16:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ CORS(app)
 @app.route('/getm3u8',methods=['GET'])
 def getm3u8():
     source = request.url
-    source = source.replace('https://1xstreamer.azurewebsites.net/getm3u8?source=', '')
+    source = source.replace('https://cdncn.herokuapp.com/getm3u8?source=', '')
     source = source.replace('%2F', '/')
     source = source.replace('%3F', '?')
     videoid = request.args.get("videoid")
@@ -26,9 +26,9 @@ def getm3u8():
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
-    ts = requests.get("http://1xstreamer.azurewebsites.net/getm3u8?source=https://edge10.xmediaget.com/hls-live/9510503/1/mediaplaylist.m3u8?s=a2f1854d23e4986571dd0883495a1580ffe58d4643e9ebe61e92bfe32a8df994&t=1672274057047&ai=3&av=1025&ui=0", headers=headers)
+    ts = requests.get("https://cdncn.herokuapp.com/getm3u8?source=https://edge10.xmediaget.com/hls-live/9510503/1/mediaplaylist.m3u8?s=a2f1854d23e4986571dd0883495a1580ffe58d4643e9ebe61e92bfe32a8df994&t=1672274057047&ai=3&av=1025&ui=0", headers=headers)
     tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://1xstreamer.azurewebsites.net/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
+    tsal = tsal.replace(videoid+'_','https://cdncn.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
 
 @app.route('/getstream',methods=['GET'])
@@ -36,7 +36,7 @@ def getstream():
     param = request.args.get("param")
     if param == "getts":
         source = request.url
-        source = source.replace('https://1xstreamer.azurewebsites.net/getstream?param=getts&source=','')
+        source = source.replace('https://cdncn.herokuapp.com/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
@@ -68,7 +68,7 @@ def getstream():
             veri = veri.replace('edge2','edge10')
             veri = veri.replace(':43434','')
             if "m3u8" in veri:
-                return "https://1xstreamer.azurewebsites.net/getm3u8?source="+veri+'&videoid='+videoid
+                return "https://cdncn.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid
         else:
             return "Veri yok"
 
