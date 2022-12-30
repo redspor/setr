7ddfba7df7248f99f9a3212a5881b1a2d9cf4e3d
redspor <121644986+redspor@users.noreply.github.com>
2022-12-30 04:20:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import requests
 from flask import Flask
 from flask import request
 import re
-from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
+
 @app.route('/getm3u8',methods=['GET'])
 def getm3u8():
     source = request.url
@@ -16,8 +15,8 @@ def getm3u8():
         "accept": "*/*",
         "accept-encoding": "gzip, deflate, br",
         "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
+        "origin": "https://lite-1x163215.top",
+        "referer": "https://lite-1x163215.top/",
         'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -26,9 +25,9 @@ def getm3u8():
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
-    ts = requests.get("https://cdncn.herokuapp.com/getm3u8?source=https://edge10.xmediaget.com/hls-live/9510503/1/mediaplaylist.m3u8?s=a2f1854d23e4986571dd0883495a1580ffe58d4643e9ebe61e92bfe32a8df994&t=1672274057047&ai=3&av=1025&ui=0", headers=headers)
+    ts = requests.get(source, headers=headers)
     tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://cdncn.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
+    tsal = tsal.replace(videoid+'_','')
     return tsal
 
 @app.route('/getstream',methods=['GET'])
@@ -37,14 +36,15 @@ def getstream():
     if param == "getts":
         source = request.url
         source = source.replace('https://cdncn.herokuapp.com/getstream?param=getts&source=','')
+        source = source.replace('https://cdncn.herokuapp.com/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
             'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'tr-TR,tr;q=0.9',
-            'origin': 'https://www.maltinok.com',
-            'referer': 'https://www.maltinok.com/',
+            'origin': 'https://lite-1x163215.top',
+            'referer': 'https://lite-1x163215.top/',
             'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
@@ -66,7 +66,6 @@ def getstream():
             veri = veri.replace('edge3','edge10')
             veri = veri.replace('edge4','edge10')
             veri = veri.replace('edge2','edge10')
-            veri = veri.replace(':43434','')
             if "m3u8" in veri:
                 return "https://cdncn.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid
         else:
