4618cdd66bce25773e1f71c0e6507d1febc8003d
redspor <121644986+redspor@users.noreply.github.com>
2023-01-01 14:45:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,26 +4,53 @@ from flask import request
 import re
 app = Flask(__name__)
 
+@app.route('/getm3u8',methods=['GET'])
+def getm3u8():
+    source = request.url
+    source = source.replace('https://testfaka.herokuapp.com/getm3u8?source=', '')
+    source = source.replace('%2F', '/')
+    source = source.replace('%3F', '?')
+    videoid = request.args.get("videoid")
+    headers = {
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "tr-TR, tr;q = 0.9",
+        "origin": "https://lite-1x163215.top",
+        "referer": "https://lite-1x163215.top/",
+        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    }
+    ts = requests.get(source, headers=headers)
+    tsal = ts.text
+    tsal = tsal.replace(videoid+'_','')
+    return tsal
+
 @app.route('/getstream',methods=['GET'])
-def getstream():  # put application's code here
+def getstream():
     param = request.args.get("param")
     if param == "getts":
         source = request.url
         source = source.replace('https://testfaka.herokuapp.com/getstream?param=getts&source=','')
+        source = source.replace('https://testfaka.herokuapp.com/getstream?param=getts&source=','')
         source = source.replace('%2F','/')
         source = source.replace('%3F','?')
         headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'tr-TR,tr;q=0.9',
+            'origin': 'https://lite-1x163215.top',
+            'referer': 'https://lite-1x163215.top/',
             'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'cross-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
         ts = requests.get(source,headers=headers)
@@ -40,24 +67,7 @@ def getstream():  # put application's code here
             veri = veri.replace('edge4','edge10')
             veri = veri.replace('edge2','edge10')
             if "m3u8" in veri:
-                headers = {
-                    "accept": "*/*",
-                    "accept-encoding": "gzip, deflate, br",
-                    "accept-language": "tr-TR, tr;q = 0.9",
-                    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-                    'sec-ch-ua-mobile': '?0',
-                    'sec-ch-ua-platform': '"Windows"',
-                    'sec-fetch-dest': 'empty',
-                    'sec-fetch-mode': 'cors',
-                    'sec-fetch-site': 'cross-site',
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-                }
-                anadomain = veri.split("mediaplaylist")
-                anadomain = anadomain[0]
-                tsal = requests.get(veri,headers=headers)
-                tsal = tsal.text.replace(videoid,anadomain+videoid)
-                '''tsal = tsal.replace('https://','https://testfaka.herokuapp.com/getstream?param=getts&source=https://')'''
-                return tsal
+                return "https://testfaka.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid
         else:
             return "Veri yok"
 
