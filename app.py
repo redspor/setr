499c901428c152edc2315ba22146a1c5c0c5aeb6
redspor <121644986+redspor@users.noreply.github.com>
2023-01-01 14:27:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def getstream():
             'sec-fetch-site': 'cross-site',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
         }
-        ts = requests.get(source,headers=headers)
+            ts = requests.get("https://coudfld.herokuapp.com/getm3u8?source=https://edge10.xmediaget.com/", headers=headers)
         return ts.content
     if param == "getm3u8":
         videoid = request.args.get("videoid")
