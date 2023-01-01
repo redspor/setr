86e28987d764dc2b284d67f5674ee02cf51d964b
redspor <121644986+redspor@users.noreply.github.com>
2023-01-01 23:38:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,33 +4,6 @@ from flask import request
 import re
 app = Flask(__name__)
 
-@app.route('/<m3u8>')
-def index(m3u8):
-    m3u8 = request.url.replace('¿','/')
-    source = m3u8
-    source = source.replace('https://teststream.herokuapp.com/', '')
-    source = source.replace('%2F', '/')
-    source = source.replace('%3F', '?')
-    videoid = request.args.get("videoid").replace('.m3u8','')
-    source = source.replace(videoid+'.m3u8',videoid)
-    headers = {
-        "accept": "*/*",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://www.maltinok.com",
-        "referer": "https://www.maltinok.com/",
-        'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
-    }
-    ts = requests.get(source, headers=headers)
-    tsal = ts.text
-    tsal = tsal.replace(videoid+'_','https://teststream.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
-    return tsal
 
 @app.route('/getm3u8',methods=['GET'])
 def getm3u8():
@@ -92,6 +65,10 @@ def getstream():
             veri = veri.replace('edge3','edge10')
             veri = veri.replace('edge4','edge10')
             veri = veri.replace('edge2','edge10')
+            veri = veri.replace('edge1','edge10')
+            veri = veri.replace('edge5','edge10')
+            veri = veri.replace('edge6','edge10')
+            veri = veri.replace('edge7','edge10')
             veri = veri.replace(':43434','')
             if "m3u8" in veri:
                 return "https://teststream.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid
