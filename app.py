b9f8b9e0dc8fb52310153ad32e9b03b5b1bf00e8
redspor <121644986+redspor@users.noreply.github.com>
2023-01-01 14:19:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import requests
 from flask import Flask
 from flask import request
 import re
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/getm3u8',methods=['GET'])
 def getm3u8():
     source = request.url
@@ -15,8 +16,8 @@ def getm3u8():
         "accept": "*/*",
         "accept-encoding": "gzip, deflate, br",
         "accept-language": "tr-TR, tr;q = 0.9",
-        "origin": "https://lite-1x163215.top",
-        "referer": "https://lite-1x163215.top/",
+        "origin": "https://www.maltinok.com",
+        "referer": "https://www.maltinok.com/",
         'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
         'sec-ch-ua-mobile': '?0',
         'sec-ch-ua-platform': '"Windows"',
@@ -25,9 +26,9 @@ def getm3u8():
         'sec-fetch-site': 'cross-site',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
     }
-    ts = requests.get(source, headers=headers)
+    ts = requests.get(source,headers=headers)
     tsal = ts.text
-    tsal = tsal.replace(videoid+'_','')
+    tsal = tsal.replace(videoid+'_','https://coudfld.herokuapp.com/getstream?param=getts&source=https://edge10.xmediaget.com/hls-live/'+videoid+'/1/'+videoid+'_')
     return tsal
 
 @app.route('/getstream',methods=['GET'])
@@ -42,8 +43,8 @@ def getstream():
             'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br',
             'accept-language': 'tr-TR,tr;q=0.9',
-            'origin': 'https://lite-1x163215.top',
-            'referer': 'https://lite-1x163215.top/',
+            'origin': 'https://www.maltinok.com',
+            'referer': 'https://www.maltinok.com/',
             'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': '"Windows"',
@@ -65,6 +66,7 @@ def getstream():
             veri = veri.replace('edge3','edge10')
             veri = veri.replace('edge4','edge10')
             veri = veri.replace('edge2','edge10')
+            veri = veri.replace(':43434','')
             if "m3u8" in veri:
                 return "https://coudfld.herokuapp.com/getm3u8?source="+veri+'&videoid='+videoid
         else:
@@ -72,3 +74,4 @@ def getstream():
 
 if __name__ == '__main__':
     app.run()
+
