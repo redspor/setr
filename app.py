b4c119d60b41e28cb0841d8730f5005470c5d320
redspor <121644986+redspor@users.noreply.github.com>
2023-02-11 12:31:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def getstream():
     if param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.11", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://lite-1x202983.top/cinema",json=veriler)
+        r = requests.post("https://lite-1x540902.top/cinema",json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
             veri = re.findall('"URL":"(.*?)"',veri)
